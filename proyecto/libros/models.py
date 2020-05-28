406fef7a4ca0820eb0c3c435980982d45862ca35
Vivi Chavez <sagitario_mixel@hotmail.com>
2020-05-27 23:41:42 -0500

    Creacion del modelo Libro

--- proyecto/libros/models.py
+++ proyecto/libros/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Libro(models.Model):
+    Titulo = models.CharField(max_length=40)
+    Autor = models.CharField(max_length=45)
+    Edicion = models.CharField(max_length=45)
+    Editorial = models.CharField(max_length=45)
+    Fecha = models.DateField()
+    NumPaginas = models.PositiveIntegerField()
