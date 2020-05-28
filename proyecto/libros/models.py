cf6b79a57c41bd666a444106f9754aeda73e4a4e
Vivi Chavez <sagitario_mixel@hotmail.com>
2020-05-27 23:54:30 -0500

    Se creo la funcion para generar la ficha bibliografica

--- proyecto/libros/models.py
+++ proyecto/libros/models.py
@@ -8,3 +8,11 @@ class Libro(models.Model):
     Editorial = models.CharField(max_length=45)
     Fecha = models.DateField()
     NumPaginas = models.PositiveIntegerField()
+
+
+   def FichaBibliografica(self):
+        cadena = "{0}, {1}, {2}, {3}, {4}"
+        return cadena.format(self.Autor,self.Titulo,self.Fecha,self.Editorial,self.NumPaginas)
+
+    def __str__(self):
+        return self.FichaBibliografica()
