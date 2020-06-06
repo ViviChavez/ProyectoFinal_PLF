31cbedab92ce103c2f152cf5cd85ec7677a661eb
Vivi Chavez <sagitario_mixel@hotmail.com>
2020-06-05 22:51:48 -0500

    Se registraron los modelos

--- proyecto/libros/admin.py
+++ proyecto/libros/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from libro.models import *
+
+
+admin.site.register(Libro)
+admin.site.register(Cliente)
+admin.site.register(Ventas)
